f1695fc7487b6f09621c9c6310d692f716b1d1de
hamza zaafrane <zaafranehamza@gmail.com>
2020-12-05 00:32:48 +0100

    second commit

--- src/main/java/com/store/bookstorespring/controllers/BookController.java
+++ src/main/java/com/store/bookstorespring/controllers/BookController.java
@@ -6,18 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-
+    // les fonctions sont invoké en form data dans postman
     @Autowired
     private BookRepository bookRepository;
 
@@ -33,58 +32,77 @@ public class BookController {
             new ResponseEntity<>(HttpStatus.ACCEPTED);
           return book.get();
         } else {
-            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<String>("Not Found",HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/allprices")
-    public double calculatePrice() {
+    public ResponseEntity<String> calculatePrice() {
     ArrayList<Book> books;
     books= (ArrayList<Book>) bookRepository.findAll();
-    double price = 0;
+    double _price = 0;
         for (Book var : books)
         {
-            price+=var.price;
+            _price+=var.getPrice();
         }
-    return price;
+        return new ResponseEntity<String>("the price of all the books is: "+_price, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public void store(Book b) {
-        try {
+    public Book store(@Validated Book b) {
             bookRepository.save(b);
-            retun new ResponseEntity<>(HttpStatus.ACCEPTED);
-        }catch (Exception e){
-            new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+            Book book =new Book(b);
+            return book;
     }
 
+    // cette method est invoké en raw json dans postman
     @RequestMapping(value = "/update/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public void update(@PathVariable Long id,@RequestBody Book bookDetails) {
+              method = RequestMethod.POST,  produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> update(@PathVariable Long id,@RequestBody @Validated Book bookDetails) {
             Optional<Book> b = bookRepository.findById(id);
             if (b.isPresent()){
                 Book book=b.get();
-                book.setAuther(bookDetails.auther);
-                book.setPrice(bookDetails.price);
-                book.setRelease(bookDetails.releasee);
-                book.setTitle(bookDetails.title);
+                book.setAuther(bookDetails.getAuther());
+                book.setPrice(bookDetails.getPrice());
+                book.setRelease_date(bookDetails.getRelease_date());
+                book.setTitle(bookDetails.getTitle());
                 bookRepository.save(book);
-                new ResponseEntity<>(HttpStatus.ACCEPTED);
+                return new ResponseEntity<String>("Book updated successfully \n"+book, HttpStatus.FOUND);
             }else{
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<String>("Book not found \n", HttpStatus.NOT_FOUND);
             }
     }
 
     @PostMapping("/delete/{id}")
-    public void deleteBook(@PathVariable long id) {
+    public ResponseEntity<String> deleteBook(@PathVariable long id) {
         try {
             bookRepository.deleteById(id);
-            new ResponseEntity<>(HttpStatus.ACCEPTED);
+            return new ResponseEntity<String>("Book deleted successfully", HttpStatus.CREATED);
         }catch (Exception e){
-            new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<String>("error"+e, HttpStatus.BAD_REQUEST);
         }
+    }
 
+    @PostMapping("/getbooklist/price")
+    public ResponseEntity<String> getBookListPrice(/*@RequestBody ArrayList<Long> bookList*/){
+        //valeurs de tests
+        int var1=12;
+        int var2=13;
+        int var3=13;
+        Long long1 = Long.valueOf(var1);
+        Long long2 = Long.valueOf(var2);
+        Long long3 = Long.valueOf(var3);
+        ArrayList<Long> bookList=new ArrayList<Long>();
+        bookList.add(long1);
+        bookList.add(long2);
+        bookList.add(long3);
+        System.out.println(bookList);
+        double _price=0;
+        for (Long var : bookList)
+        {
+            Optional<Book> b = bookRepository.findById(var);
+            _price+=b.get().getPrice();
+        }
+        return new ResponseEntity<String>("the price of the given booklist: "+_price, HttpStatus.OK);
     }
 }
--- src/main/java/com/store/bookstorespring/entities/Book.java
+++ src/main/java/com/store/bookstorespring/entities/Book.java
@@ -1,60 +1,81 @@
 package com.store.bookstorespring.entities;
 
+
+import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import java.io.Serializable;
 
+
 @Entity
-@Table(name="books")
 public class Book implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    @Column
-    public String title;
-    @Column
-    public String auther;
-    @Column
-    public double price;
-    @Column
-    public int releasee;
+    private Long id;
+    @Length(min = 4, max = 15)
+    private String title;
+    @Length(min = 4, max = 15)
+    private String auther;
+    private double price;
+    @Length(min = 4, max = 4)
+    private String release_date;
 
-    public Book(Book b) {
-        this.id = b.id;
-        this.title = b.title;
-        this.auther = b.auther;
-        this.price = b.price;
-        this.releasee = b.releasee;
+    public Long getId() {
+        return id;
     }
 
-    public Book(Long id, String title, String auther, double price, int release) {
+    public void setId(Long id) {
         this.id = id;
-        this.title = title;
-        this.auther = auther;
-        this.price = price;
-        this.releasee = release;
     }
 
-    public Book() {
-
+    public String getTitle() {
+        return title;
     }
-
-
-
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getAuther() {
+        return auther;
     }
 
     public void setAuther(String auther) {
         this.auther = auther;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
     }
 
-    public void setRelease(int release) {
-        this.releasee = release;
+    public Book(Long id, String title, String auther, double price, String release_date) {
+        this.id = id;
+        this.title = title;
+        this.auther = auther;
+        this.price = price;
+        this.release_date = release_date;
+    }
+
+    public Book(Book b) {
+        this.id = b.id;
+        this.title = b.title;
+        this.auther = b.auther;
+        this.price = b.price;
+        this.release_date = b.release_date;
+    }
+
+    public Book() {
+
+    }
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
     }
 
     @Override
@@ -64,7 +85,7 @@ public class Book implements Serializable {
                 ", title='" + title + '\'' +
                 ", auther='" + auther + '\'' +
                 ", price=" + price +
-                ", release=" + releasee +
+                ", release=" + release_date +
                 '}';
     }
 
